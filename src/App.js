ac97a0a904f1021708472c899b2e763c63282d6d
cornettj07 <jaspersemail07@gmail.com>
2017-07-18 17:55:46 -0400

    added that navbar, bro.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import uvalogo from './UvaLogo.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className='cool-header'>
+          <div>
+            <img style={{width: 324, height: 35 }} src={uvalogo}/>
+          </div>
+         <ul id="menu">
+          <li><a href="">Getting Started</a>
+            <ul>
+              <li><a href="">Apple</a></li>
+              <li><a href="">Android</a></li>
+            </ul>
+          </li>
+          <li><a href="">Dev Resources</a>
+            <ul>
+              <li><a href="">API References</a></li>
+              <li><a href="">Demo Apps</a></li>
+              <li><a href="">External Resources</a></li>
+              <li><a href="">Videos</a></li>
+            </ul>
+          </li>
+          <li><a href="">Community</a>
+            <ul>
+              <li><a href="">Blog</a></li>
+              <li><a href="">Student Apps and Projects</a></li>
+            </ul>
+          </li>
+          <li><a href="">Signup / Login</a></li>
+        </ul>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
     );
   }
 }
