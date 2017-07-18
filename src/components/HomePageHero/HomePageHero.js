dfbe35bf935f1fd5c75814b97d5c27d89322436c
Jake Pitner <jacobpitner11@gmail.com>
2017-07-18 17:51:12 -0400

    added some things like image and description

---
+++ src/components/HomePageHero/HomePageHero.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import "./HomePageHero.css";
+
+class HomePageHero extends Component {
+  render() {
+    return (
+        <div className="hero-image">
+          <div className="text-container">
+            <h1 className="hero-image-header"> About Us </h1>
+            <div className="body-text">
+              <p1>
+                UVA Dev Hub is an opportunity for all members of the University
+                of
+                <br />Virginia community to collaborate on applications and
+                solutions that
+                <br />benefit the campus.
+              </p1>
+            </div>
+          </div>
+          <div className="middle">
+            nibba
+          </div>
+        </div>
+    );
+  }
+}
+
+export default HomePageHero;
